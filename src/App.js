140e6df419d3a8fd42b2b3e2d6f02e9c79850cdb
TadeoDaniel19 <hector_lectric@hotmail.com>
2020-05-13 17:07:39 -0500

    refactoring app.js and some changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React from 'react' 
-import { Cards, CountryPicker, Chart } from './Components' 
-import { fetchData } from './api/' 
-import styles from './App.module.css' 
-import image from './images/image.png' 
+import React, { useEffect, useState } from 'react'
+import { Cards, CountryPicker, Chart } from './Components'
+import { fetchData } from './api/'
+import styles from './App.module.css'
+import image from './images/image.png'
 
-class App extends React.Component {
-  state = {
+const App = () => {
+
+  const [state, setState] = useState({
     data: {},
-    country: '',
+    country: ''
+  })
+
+  useEffect(() => {
+    const fetchCall = async () => {
+      const data = await fetchData()
+      setState({ data })
+    }
+
+    fetchCall()
+  }, [])
+
+  const handleCountryChange = async (country) => {
+    const data = await fetchData(country)
+
+    setState({ data, country: country })
   }
 
-  async componentDidMount() {
-    const data = await fetchData() 
+  const { data, country } = state
 
-    this.setState({ data }) 
-  }
-
-  handleCountryChange = async (country) => {
-    const data = await fetchData(country) 
-
-    this.setState({ data, country: country }) 
-  }
-
-  render() {
-    const { data, country } = this.state 
-
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    ) 
-  }
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt="COVID-19" />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+    </div>
+  )
 }
 
 export default App 
